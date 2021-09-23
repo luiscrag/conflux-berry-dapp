da37f4c840ac87cffdaac74f15ab94738b87cf8c
luiscrag <luiscragdev@protonmail.com>
2021-09-23 16:09:29 -0500

    Add Toaster Notification

--- src/Providers.jsx
+++ src/Providers.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import {ModalProvider} from "./contexts";
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 const Providers = ({ children }) => (
   <ModalProvider>
+    <ToastContainer theme="dark"/>
     {children}
   </ModalProvider>
 );
