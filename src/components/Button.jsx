63a27359ad8cf570b1e80cac73bd203d7e629ca6
luiscrag <luiscragdev@protonmail.com>
2021-09-26 13:23:18 -0500

    initialIcon Props Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "../styles/components/Button.scss";
 
-const Button = ({ children, ...props }) => {
+const Button = ({ initialIcon, children, ...props }) => {
   const { action, disabled } = props;
   return (
     <button
       onClick={action}
       id="ActionButton"
-      className={disabled ? "Disabled" : "None"}
+      className={`${disabled ? "Disabled" : "None"} ${initialIcon ? "Flex" : "none"}`}
     >
+      {initialIcon && initialIcon}
       {children}
     </button>
   );
